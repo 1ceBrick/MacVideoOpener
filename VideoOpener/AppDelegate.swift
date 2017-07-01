07822e1d61676b931937d1632b0726d7a3d0bb35
Stas Chmilenko <staschmilenko@gmail.com>
2017-07-01 16:30:41 +0300

    Initial Commit

---
+++ VideoOpener/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoOpener
+//
+//  Created by Stas Chmilenko on 01.07.17.
+//  Copyright © 2017 Stas Chmilenko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VideoOpener/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VideoOpener
+//
+//  Created by Stas Chmilenko on 01.07.17.
+//  Copyright © 2017 Stas Chmilenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
